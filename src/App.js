b5fd5be60123ec8a0d1c7b7548c3b259a51f9d9e
Andrea Bentley <andrea@bentleytech.com>
2020-07-26 15:51:13 -0400

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -26,26 +26,26 @@ function App() {
   }, [pathState]);
 
   return (
-    <BrowserRouter basename={pathState}>
+    <HashRouter basename={pathState}>
       <div>
         <Nav />
         <Switch>
-          {/* <Route exact path="/About"><About /></Route>
+          <Route exact path="/About"><About /></Route>
           <Route exact path="/Portfolio"><Portfolio /></Route>
           <Route exact path="/Contact"><Contact /></Route>
-          <Route path="/*"><About /></Route> */}
+          <Route path="/*"><About /></Route>
           {/* <Route exact path="/About" component={About} />
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/Contact" component={Contact} />
           <Route path="/*" component={About} /> */}
-          <Route exact path="/About" render={() => <About />} />
+          {/* <Route exact path="/About" render={() => <About />} />
           <Route exact path="/Portfolio" render={() => <Portfolio />} />
           <Route exact path="/Contact" render={() => <Contact />} />
-          <Route path="/*" render={() => <About />} />
+          <Route path="/*" render={() => <About />} /> */}
         </Switch>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
