65eff26756e08b83622332925bd20be09f671cd8
Andrea Bentley <andrea@bentleytech.com>
2020-07-26 16:34:57 -0400

    still fixing routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
@@ -31,18 +32,18 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/About"><About /></Route>
+          {/* <Route exact path="/About"><About /></Route>
           <Route exact path="/Portfolio"><Portfolio /></Route>
           <Route exact path="/Contact"><Contact /></Route>
-          <Route path="/*"><About /></Route>
+          <Route path="/*"><About /></Route> */}
           {/* <Route exact path="/About" component={About} />
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/Contact" component={Contact} />
           <Route path="/*" component={About} /> */}
-          {/* <Route exact path="/About" render={() => <About />} />
+          <Route exact path="/About" render={() => <About />} />
           <Route exact path="/Portfolio" render={() => <Portfolio />} />
           <Route exact path="/Contact" render={() => <Contact />} />
-          <Route path="/*" render={() => <About />} /> */}
+          <Route path="/*" render={() => <About />} />
         </Switch>
         <Footer />
       </div>
