fac22189507ae69dcfe83b580379e84bd0293a10
Andrea Bentley <andrea@bentleytech.com>
2020-08-17 21:45:38 -0400

    Updated About and resume

--- src/pages/About.js
+++ src/pages/About.js
@@ -24,14 +24,14 @@ function About() {
                 <figcaption>Resume</figcaption>
               </Figure>
             </Anchor>
-            <Paragraph>An experienced software developer, looking to enter the world of web
-              development. Currently expanding the knowledge gained earning a B.S. in Information and
-              Computer Science from the Georgia Institute of Technology by taking their Coding Boot Camp.
-              In August 2020, will receive a Certificate of Completion in Web Development from Georgia
-              Tech Professional Education. Professional experience in object-oriented programming, rapid
-              application development tools, and collaborating with a team to create a finished product.
-              Main area of interest is UI/UX, with strong analytical skills that are well-suited for
-              full-stack development.
+            <Paragraph>An experienced software developer, looking to enter the world of web development. 
+              Expanded the knowledge gained earning a B.S. in Information and Computer Science from the 
+              Georgia Institute of Technology by taking their Coding Boot Camp. Just received a Certificate of 
+              Completion in Web Development from Georgia Tech Professional Education. Professional experience in 
+              object-oriented programming, rapid application development tools, and collaborating with a team 
+              to create a finished product. Main area of interest is UI/UX, with strong analytical skills that 
+              are well-suited for full-stack development.s UI/UX, with strong analytical skills that are 
+              well-suited for full-stack development.
             </Paragraph>
           </Column>
         </Row>
