1200d74603ef2e129cc05197d30ee483015fa840
Andrea Bentley <andrea@bentleytech.com>
2020-10-26 20:45:59 -0400

    Updates

---
+++ precache-manifest.e24a897960d25305190ff3922e3e81a4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092932f943576c1b252c51ec4c15da67",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "33c8372d34b6eb929dae",
+    "url": "/React-Portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "f20935170185d91c3245",
+    "url": "/React-Portfolio/static/css/main.7392a778.chunk.css"
+  },
+  {
+    "revision": "33c8372d34b6eb929dae",
+    "url": "/React-Portfolio/static/js/2.99c0fa5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.99c0fa5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20935170185d91c3245",
+    "url": "/React-Portfolio/static/js/main.c53b537a.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "e702f43ded2e81cf9d9b9f69f7332ae4",
+    "url": "/React-Portfolio/static/media/Andrea-Bentley-Resume.e702f43d.pdf"
+  },
+  {
+    "revision": "0f6bf54254171d014a37890a41bd35e9",
+    "url": "/React-Portfolio/static/media/Background-gray-textured.0f6bf542.jpg"
+  },
+  {
+    "revision": "42cfec486e5fa82da3d96a51d5e482c0",
+    "url": "/React-Portfolio/static/media/Budget-Tracker.42cfec48.png"
+  },
+  {
+    "revision": "9cc9d28edf93b58982a59549351de25c",
+    "url": "/React-Portfolio/static/media/Coding-Quiz.9cc9d28e.png"
+  },
+  {
+    "revision": "d2e84bbb6a8580749756f98689c8d3e4",
+    "url": "/React-Portfolio/static/media/Employee-Directory.d2e84bbb.png"
+  },
+  {
+    "revision": "1cd3fc50f7af2fc72a1bf3b2b48f68d4",
+    "url": "/React-Portfolio/static/media/Employee-Tracker.1cd3fc50.png"
+  },
+  {
+    "revision": "2d140604cc575b8f8d614940d0cb4360",
+    "url": "/React-Portfolio/static/media/Employee-Tracker.2d140604.gif"
+  },
+  {
+    "revision": "cfef8ef14346745669898d72670d43bc",
+    "url": "/React-Portfolio/static/media/Google-Books.cfef8ef1.png"
+  },
+  {
+    "revision": "209ed3575aa8561f27ebc8a4068598bd",
+    "url": "/React-Portfolio/static/media/Note-Taker.209ed357.png"
+  },
+  {
+    "revision": "6170604c9c66a7a346cae92a9da72632",
+    "url": "/React-Portfolio/static/media/Park-Planner.6170604c.png"
+  },
+  {
+    "revision": "18e0efbf0bfb58de8ea81a565daece40",
+    "url": "/React-Portfolio/static/media/Password-Generator.18e0efbf.png"
+  },
+  {
+    "revision": "a76f85bc7b9920b45c53b7adde1833c3",
+    "url": "/React-Portfolio/static/media/Profile.a76f85bc.jpg"
+  },
+  {
+    "revision": "00d2c39f69da3c0e8baf4394f4732198",
+    "url": "/React-Portfolio/static/media/Readme-Generator.00d2c39f.gif"
+  },
+  {
+    "revision": "e743cee4db4b77a7664357c3167001ff",
+    "url": "/React-Portfolio/static/media/Readme-Generator.e743cee4.png"
+  },
+  {
+    "revision": "14b0a513044216958e56a4a5a2b9e287",
+    "url": "/React-Portfolio/static/media/Recipe-Xchange.14b0a513.png"
+  },
+  {
+    "revision": "2f6f19e63a03351f147fefd860b1c8bb",
+    "url": "/React-Portfolio/static/media/Task-Manager.2f6f19e6.png"
+  },
+  {
+    "revision": "60571f0e1a39200ce9583055298968d2",
+    "url": "/React-Portfolio/static/media/Team-Template-Generator.60571f0e.gif"
+  },
+  {
+    "revision": "d6b2499952cf6f88988e54feeff1d7e4",
+    "url": "/React-Portfolio/static/media/Team-Template-Generator.d6b24999.png"
+  },
+  {
+    "revision": "fbc129c7f7af7c24e3e17e07e075b26e",
+    "url": "/React-Portfolio/static/media/Weather-Dashboard.fbc129c7.png"
+  },
+  {
+    "revision": "703b1f1798baf7f6f351e9b026480fb4",
+    "url": "/React-Portfolio/static/media/Work-Day-Scheduler.703b1f17.png"
+  },
+  {
+    "revision": "316349feec5e18dca7c1c4ac8f3fc46c",
+    "url": "/React-Portfolio/static/media/Workout-Tracker.316349fe.png"
+  }
+]);
